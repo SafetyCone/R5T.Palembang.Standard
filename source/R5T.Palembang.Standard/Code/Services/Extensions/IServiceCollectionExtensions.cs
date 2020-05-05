1925c3afeb5c5a432948176ac77d5b664e6c0a25
MinexAutomation <minex.automation@gmail.com>
2020-05-05 15:48:41 -0700

    Quick code trivia cleanup.

--- source/R5T.Palembang.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Palembang.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace R5T.Palembang.Standard.Code.Services.Extensions
+
+namespace R5T.Palembang.Standard
 {
     class IServiceCollectionExtensions
     {
